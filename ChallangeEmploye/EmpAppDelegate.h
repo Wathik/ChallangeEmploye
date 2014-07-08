4e8098c3306f726d0b34fdfc52e0e506d61a3b9e
Wathik Almayali <wathikalmayali@Wathik-Almayali.local>
2014-07-07 20:51:23 -0400

    Initial Commit

---
+++ ChallangeEmploye/EmpAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EmpAppDelegate.h
+//  ChallangeEmploye
+//
+//  Created by Wathik Almayali on 7/7/14.
+//  Copyright (c) 2014 Wathik Almayali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EmpAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ChallangeEmploye/EmpViewController.m
@@ -0,0 +1,29 @@
+//
+//  EmpViewController.m
+//  ChallangeEmploye
+//
+//  Created by Wathik Almayali on 7/7/14.
+//  Copyright (c) 2014 Wathik Almayali. All rights reserved.
+//
+
+#import "EmpViewController.h"
+
+@interface EmpViewController ()
+
+@end
+
+@implementation EmpViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
